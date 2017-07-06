a2233869d79671966d46e1574ef205ab296d356f
Emily Boucher <emi.boucher@gmail.com>
2017-07-06 16:57:36 -0400

    added else statemnt turning light back on; light switch rave is a go

--- js/main.js
+++ js/main.js
@@ -11,9 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (body.classList.contains('light')) {
       body.classList.remove('light');
       body.classList.add('dark');
-      message.innerText = ('Hey, who turned off the lights?')
+      message.innerText = ('Hey, who turned off the lights?');
       console.log('Hey, who turned off the lights?');
-    };
+    } else if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+        body.classList.add('light');
+        message.innerText = ("It's so bright in here!");
+        console.log("It's so bright in here!");
+      };
   });
 
 });
