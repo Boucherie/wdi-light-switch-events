2d9f9eaf8b1579d4e2d114645cdffabae97f6e93
Emily Boucher <emi.boucher@gmail.com>
2017-07-06 16:44:48 -0400

    created body, lightSwitch (.plate), message(h1) variables, first part of is statement turning off lights and changing h1 msg

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
+document.addEventListener('DOMContentLoaded', function() {
 
+  var body = document.querySelector('body');
+  //var light = document.querySelector('.light');
+  var lightSwitch = document.querySelector('button');
+
+  var message = document.querySelector('h1');
+
+  lightSwitch.addEventListener('click', function() {
+    if (body.classList.contains('light')) {
+      body.classList.remove('light');
+      body.classList.add('dark');
+      message.innerText = ('Hey, who turned off the lights?')
+      console.log('Hey, who turned off the lights?');
+    };
+  });
+
+});
